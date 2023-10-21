3881b103adbf3008881b0e9e3e0c44eaa03de77e
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2023-10-21 04:34:08 +0100

    UI update

--- frontend/components/dashboard/CompanyView.tsx
+++ frontend/components/dashboard/CompanyView.tsx
@@ -109,7 +109,7 @@ export const CompanyInfo = ({
         <div className="avatar avatar-ring-primary mr-5">
           <img src={`https://ipfs.io/ipfs/${groupLogo}`} alt="avatar" />
         </div>
-        <span className="font-bol text-xl py-5 mb-5 pb-5"> {groupName} </span>
+        <span className="font-bol text-xl py-5 mb-5 pb-5"> {groupName} TechFusion </span>
 
         <div className="font-bol text-xl py-5 mb-5 pb-5"> Members</div>
 
@@ -136,7 +136,7 @@ export const CompanyInfo = ({
           </table>
         </div>
         <div className=" mt-7 divider my-0"></div>
-        <div className="font-bol text-xl py-5 mb-5 pb-2"> SET DIMENSIONs</div>
+        <div className="font-bol text-xl py-5 mb-5 pb-2"> Add Employee</div>
 
         <form>
           <div>
@@ -148,8 +148,17 @@ export const CompanyInfo = ({
               onChange={(e) => setDimensionName(e.target.value)}
             />
           </div>
-          <div>
-            <label>Weight:</label>
+          <div className="mt-2">
+            <label>Address:</label>
+            <input
+              className="py-2 px-3 pr-11 block   border-gray-200 shadow-sm -mt-px -ml-px first:rounded-t-lg last:rounded-b-lg sm:last:rounded-r-lg text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
+              type="text"
+              value={dimensionName}
+              onChange={(e) => setDimensionName(e.target.value)}
+            />
+          </div>
+          <div className="mt-2">
+            <label>Wage(monthly):</label>
             <input
               className="py-2 px-3 pr-11 block  border-gray-200 shadow-sm -mt-px -ml-px first:rounded-t-lg last:rounded-b-lg sm:last:rounded-r-lg text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
               type="number"
@@ -162,57 +171,54 @@ export const CompanyInfo = ({
             onClick={handleAddDimension}
             className="py-2 mt-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
           >
-            Add Dimension
+            Add Employee
           </button>
         </form>
 
-        {names.length > 0 && (
-          <>
-            <h2 className="font-bold mt-1">Added Dimensions</h2>
-            <ul>
-              {names.map((name: any, index: any) => (
-                <li key={index}>
-                  Name: {name} -- Weight: {weights[index]}
-                </li>
-              ))}
-            </ul>
-            <button
-              type="button"
-              onClick={handleCreateDimensions}
-              className="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
-            >
-              Create Dimensions
-            </button>
-          </>
-        )}
-
         <div className=" mt-7 divider my-0"></div>
-        <div className="font-bol text-xl py-5 mb-5 pb-5">
+        <div className="font-bol text-xl py-5 mb-3 pb-5">
           {" "}
-          DISTRIBUTION AND EVALUATION RESULTS
+          Your Available Wallet Balance: <strong> 0 USDT</strong> 
         </div>
 
         <div className="flex w-full overflow-x-auto">
-          <table className="table-zebra table">
-            <thead>
-              <tr>
-                <th>SN</th>
-                <th>Address</th>
-                <th>Percentage(%)</th>
-                <th>Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th>1</th>
-                <td>Ox...90</td>
-                <td>40</td>
-                <td>100 $</td>
-              </tr>
-            </tbody>
-          </table>
+          
+          
+        <div>
+             <button
+            type="button"
+            onClick={handleAddDimension}
+            className="py-2 ml-2 mt-3  px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-gradient-to-tl from-blue-600 to-yellow-600 shadow-lg shadow-transparent hover:shadow-blue-700/50 border border-transparentfocus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+          >
+          Withdraw Salary
+          </button>
+          </div>
         </div>
+        <div className=" mt-7 divider my-0"></div>
+
+        <div className="flex w-full overflow-x-auto">
+          
+          
+        <div>
+            <label className="ml-2">Loan Amount:</label>
+            <input
+              className="py-2 px-3 pr-11 block ml-2 mt-1  border-gray-200 shadow-sm -mt-px -ml-px first:rounded-t-lg last:rounded-b-lg sm:last:rounded-r-lg text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
+              type="text"
+              value={dimensionName}
+              onChange={(e) => setDimensionName(e.target.value)}
+            />
+             <button
+            type="button"
+            onClick={handleAddDimension}
+            className="py-2 ml-2 mt-3 mt-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+          >
+           Request Loan
+          </button>
+          </div>
+        </div>
+        <div className=" mt-7 divider my-0"></div>
       </div>
+      
     </>
   );
 };
