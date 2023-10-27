6d3736a58871e2044f7a11f99266755b8065e431
Gabriel Temsten <57184013+gabrieltemtsen@users.noreply.github.com>
2023-10-27 21:56:45 +0100

    test

--- frontend/components/index.tsx
+++ frontend/components/index.tsx
@@ -2,3 +2,6 @@ export { default as Navbar } from "./Navbar";
 export { default as Hero } from "./Hero";
 export { default as Footer } from "./Footer";
 export { default as Features } from "./Features";
+
+// trying to 
+
